f971e08cd40af621741bb514b21c62adb34d778a
Edsano <edsonmatheus02@hotmail.com>
2023-05-20 18:51:26 -0300

    conteúdos criados e dinamizados

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "ARTISTA DIGITAL DESCOBERTO",
+    "description": "Novo artista é descoberto nas redes sociais, mostrando belíssimas artes digitais que ele tem feito. Confira!",
+    "pictureCover": "https://static.vecteezy.com/ti/vetor-gratis/p1/2849268-pro-digital-artist-creation-sketch-vetor.jpg"
+  },
+  {
+    "id": "2",
+    "title": "DESENVOLVIMENTO FRONT-END",
+    "description": "Descubra como se tornar um bom desenvolvedor Frontend e conquistar as melhores vagas do mercado de tecnologia!",
+    "pictureCover": "https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8ZnJvbnQlMjBlbmR8ZW58MHx8MHx8fDA%3D&w=1000&q=80"
+  }
+];
