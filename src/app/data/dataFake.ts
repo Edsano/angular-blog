e5341b2a92d83a3ecb201773e24c3b3c252732fd
Edsano <edsonmatheus02@hotmail.com>
2023-05-20 20:21:05 -0300

    adicionados o conteúdo dos demais componentes no arquivo dataFake.ts

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -10,5 +10,23 @@ export const dataFake = [
     "title": "DESENVOLVIMENTO FRONT-END",
     "description": "Descubra como se tornar um bom desenvolvedor Frontend e conquistar as melhores vagas do mercado de tecnologia!",
     "pictureCover": "https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8ZnJvbnQlMjBlbmR8ZW58MHx8MHx8fDA%3D&w=1000&q=80"
+  },
+  {
+    "id": "3",
+    "title": "DESENVOLVIMENTO WEB",
+    "description": "Sistemas Web e suas vantagens tecnológicas no mercado.",
+    "pictureCover": "https://e1.pxfuel.com/desktop-wallpaper/125/969/desktop-wallpaper-web-development-posted-by-sarah-anderson-full-stack.jpg"
+  },
+  {
+    "id": "4",
+    "title": "O FILME DO SUPER MARIO",
+    "description": "Novo filme sobre a aventura do incrível Super Mario alegra os corações de jogadores da Nintendo.",
+    "pictureCover": "https://cdn.vox-cdn.com/thumbor/HyeCibJodUnXUPCGXRL5JXNh1Jg=/0x0:3588x1500/1200x800/filters:focal(1507x463:2081x1037)/cdn.vox-cdn.com/uploads/chorus_image/image/72297223/2530_T2_00041.0.jpg"
+  },
+  {
+    "id": "5",
+    "title": "OS FAMOSOS BITCOINS",
+    "description": "Acompanhe de perto o que está acontecendo no mundo da mineração de moedas tecnológicas.",
+    "pictureCover": "https://s2.glbimg.com/DS4shch9wwDQrz5Ks2x39_FHaNc=/620x430/bottom/e.glbimg.com/og/ed/f/original/2018/01/08/pexels-bitcoin.jpeg"
   }
 ];
